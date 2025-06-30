2ddee977953f93cf2730768534aa11878895e267
Dev Karan Singh <dev1604karan@gmail.com>
2025-06-30 08:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // Function to find equilibrium point in the array.
+    int findEquilibrium(vector<int> &arr) {
+        
+        int n = arr.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        
+        int left = 0;
+        for(int i = 0; i < n; i++){
+            int right = sum - left - arr[i];
+            if(left == right) return i;
+            left += arr[i];
+        }
+        return -1;
+    }
+};
