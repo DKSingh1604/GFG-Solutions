410058665355850600c27a8c4ae2361fb423a33d
Dev Karan Singh <dev1604karan@gmail.com>
2025-07-03 22:09:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -1,34 +1,20 @@
-// User function template for C++
 class Solution {
   public:
-    // Function returns the second
-    // largest elements
     int getSecondLargest(vector<int> &arr) {
-        // Code Here
+        // code here
         int n = arr.size();
-        
-        int max1 = 1;
-        int max2 = 1;
-        
+        int h = -1;
+        int sh = -1;
         for(int i = 0; i < n; i++){
-            if(arr[i] > max1){
-                max2 = max1;
-                max1 = arr[i];
-                
+            if(arr[i] > h){
+                sh = h;
+                h = arr[i];
             }
-            else if((arr[i] < max1 && arr[i] > max2) ){
-                max2 = arr[i];
+            else if(arr[i] > sh && arr[i] < h){
+                sh = arr[i];
             }
-            
         }
-        if(max2 == 1){
-            return -1;
-        }
-        else {
-            return max2;
-        }
-        
-        
+        return sh;
         
     }
 };
