221f8dec7100af37a0f9f5922094377d65bbaa48
Dev Karan Singh <dev1604karan@gmail.com>
2025-07-20 14:25:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.cpp
@@ -0,0 +1,49 @@
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        Node* slow = head;
+        Node* fast = head;
+        
+        while(fast -> next != NULL && fast -> next -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast){
+                slow = head;
+                break;
+            }
+            if(fast == NULL || fast -> next == NULL || fast -> next -> next == NULL) {
+                return;
+            }
+        }
+        
+        while(slow != fast){
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+        
+        while(fast -> next != slow){
+            fast = fast -> next;
+        }
+        
+        fast -> next = NULL;
+    }    
+};
