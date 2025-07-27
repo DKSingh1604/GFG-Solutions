9c8e5f98e5d06ee309eb093046ed0e9c2f3215f9
Dev Karan Singh <dev1604karan@gmail.com>
2025-07-27 09:44:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -4,22 +4,43 @@ class Solution {
         // code here
         
         int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-    
-        for(int i = n - 1; i >= 0; i--) {
-            // Clean up smaller or equal elements
-            while(!st.empty() && st.top() <= arr[i]) {
-                st.pop();
-            }
-    
-            // If stack is empty, no greater element
-            ans[i] = st.empty() ? -1 : st.top();
-    
-            // Push current element to stack for future comparisons
-            st.push(arr[i]);
+        
+        vector<int> ans;
+        
+        if(n == 1){
+            ans.push_back(-1);
+            return ans;
         }
-    
+        
+        stack<int> st;
+        
+        for(int i = n-1; i >= 0; i--){
+            if(i == n-1){
+                st.push(arr[i]);
+                ans.push_back(-1);
+            }
+            else{
+                if(st.top() <= arr[i]){
+                    while(!st.empty() && st.top() <= arr[i]){
+                        st.pop();
+                    }
+                    if(st.empty()){
+                        ans.push_back(-1);
+                    }
+                    else{
+                        ans.push_back(st.top());
+                    }
+                    st.push(arr[i]);
+                }
+                else{
+                    ans.push_back(st.top());
+                    st.push(arr[i]);
+                }
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
         return ans;
         
     }
