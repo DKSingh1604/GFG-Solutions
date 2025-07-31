df1af2e332e7ef9a6f955b762e98daa30077e297
Dev Karan Singh <dev1604karan@gmail.com>
2025-07-31 18:00:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
@@ -0,0 +1,55 @@
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+        // Your Code
+        
+    QueueNode* temp = new QueueNode(x);
+    
+    if(front == NULL){
+        front = temp;
+        rear = front;
+    }
+    else{
+        rear -> next = temp;
+        rear = temp;
+    }
+    return;
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code 
+        
+        if(front == NULL){
+            return -1;
+        }
+        else{
+            QueueNode* temp = front;
+            front = front -> next;
+            temp -> next = NULL;
+            return temp -> data;
+            
+        }
+}
