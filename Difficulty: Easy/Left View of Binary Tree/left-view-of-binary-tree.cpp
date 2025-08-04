1ef4c322ac0d2a3a871aca88ff6d2f82c5b38a74
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-04 22:17:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -0,0 +1,33 @@
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    void  getvec( vector<int>&ans,Node*root,int level){
+      if(!root)return;
+      if(level==ans.size()){
+          ans.push_back(root->data);
+      }
+      getvec(ans,root->left,level+1);
+      getvec(ans,root->right,level+1);
+  }
+    vector<int> leftView(Node *root) {
+        // code here
+        
+          vector<int>ans;
+          getvec(ans,root,0);
+          return ans;
+    }
+};
