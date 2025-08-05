fefa813cb87e70627106e27774cdd6690ab08bd3
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-05 16:27:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.cpp
+++ Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.cpp
@@ -19,22 +19,34 @@ Node* newNode(int val) {
 
 class Solution {
   public:
-    int height(Node* root, int&maxi){
-        if(root == NULL) return 0;
+    pair<int, int> diameterFast(Node* root){
         
-        int left = height(root -> left, maxi);
-        int right = height(root -> right, maxi);
         
-        maxi = max(maxi, left + right);
+        if(!root) {
+            pair<int, int> p = make_pair(0, 0);
+            return p;
+        }
         
-        return 1 + max(left, right);
+        pair<int, int> left = diameterFast(root -> left);
+        pair<int, int> right = diameterFast(root -> right);
+        
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        
+        int op3 = left.second + right.second;
+        
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second, right.second) + 1;
+        
+        return ans;
+        
     }
-    
     int diameter(Node* root) {
-        int maxi = 0;
-        height(root, maxi);
+        // Your code here
+        return diameterFast(root).first;
         
-        return maxi;
         
     }
 };
