038240946881194b01d662ced41df672a927cea8
Dev Karan Singh <dev1604karan@gmail.com>
2025-09-24 19:02:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void dfs(int node, vector<vector<int>> adjList, int vis[]){
+          vis[node] = 1;
+          for(auto it : adjList[node]){
+              if(!vis[it]){
+                  dfs(it, adjList, vis);
+              }
+          }
+          
+      }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        vector<vector<int>> adjList(V);
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                if(adj[i][j] == 1 && i != j){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            }
+        }
+        
+        int vis[V] = {0};
+        int count = 0;
+        for(int i = 0; i < V; i++){
+            
+            if(!vis[i]){
+                count++;
+                dfs(i, adjList, vis);
+            }
+        }
+        
+        return count;
+    }
+};
