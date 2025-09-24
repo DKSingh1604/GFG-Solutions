a545d33291df0affb1be859bf977205c5d5ec18c
Dev Karan Singh <dev1604karan@gmail.com>
2025-09-24 18:05:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void dfsOfGraph(int node, vector<vector<int>>& adj, int vis[], vector <int> &ls){
+        vis[node] = 1;
+        ls.push_back(node);
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfsOfGraph(it, adj, vis, ls);
+            }
+        }
+    }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int n = adj.size();
+        int vis[n] = {0};
+        int start = 0;
+        vector<int> ls;
+        dfsOfGraph(start, adj, vis, ls);
+        return ls;
+    }
+};
