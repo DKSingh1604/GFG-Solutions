197b2bceda75685fef107e982d5e20e51b21ce56
Dev Karan Singh <dev1604karan@gmail.com>
2025-10-02 19:21:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,29 +1,53 @@
 class Solution {
   public:
-    bool dfsCheck(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &pathVis){
+  //--------------Using DFs --------------------------------------------------
+    // bool dfsCheck(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &pathVis){
         
-        vis[node] = 1;
-        pathVis[node] = 1;
+    //     vis[node] = 1;
+    //     pathVis[node] = 1;
         
-        for(auto it : adj[node]){
-            //if node not visited
-            if(!vis[it]){
-                if(dfsCheck(it, adj, vis, pathVis)) return true;
-            }
+    //     for(auto it : adj[node]){
+    //         //if node not visited
+    //         if(!vis[it]){
+    //             if(dfsCheck(it, adj, vis, pathVis)) return true;
+    //         }
             
-            //if node is visited
-            else if(pathVis[it]){
-                return true;
-            }
-        }
+    //         //if node is visited
+    //         else if(pathVis[it]){
+    //             return true;
+    //         }
+    //     }
         
-        pathVis[node] = 0;
-        return false;
+    //     pathVis[node] = 0;
+    //     return false;
                
-    }
-    bool isCyclic(int V, vector<vector<int>> &edges) {
-        // code here
+    // }
+    // bool isCyclic(int V, vector<vector<int>> &edges) {
+    //     // code here
         
+    //     vector<vector<int>> adj(V);
+    //     for(auto edge : edges){
+    //         int u = edge[0];
+    //         int v = edge[1];
+    //         adj[u].push_back(v);
+    //     }
+        
+    //     vector<int> vis(V, 0);
+    //     vector<int> pathVis(V, 0);
+        
+        
+    //     for(int i = 0; i < V; i++){
+    //         if(!vis[i]){
+    //             if(dfsCheck(i, adj, vis, pathVis)) return true;
+    //         }
+    //     }
+        
+    //     return false;
+    // }
+    
+    //-------------------------Using BFS---------------------------------------
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+    
         vector<vector<int>> adj(V);
         for(auto edge : edges){
             int u = edge[0];
@@ -31,16 +55,40 @@ class Solution {
             adj[u].push_back(v);
         }
         
-        vector<int> vis(V, 0);
-        vector<int> pathVis(V, 0);
+        int inDegree[V] = {0};
         
-        
+        //made the inDegree array
         for(int i = 0; i < V; i++){
-            if(!vis[i]){
-                if(dfsCheck(i, adj, vis, pathVis)) return true;
+            for(auto it : adj[i]){
+                inDegree[it]++;
             }
         }
         
-        return false;
+        //push the elements with 0 in degree into the queue
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0){
+               q.push(i); 
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        if(topo.size() == V){
+            return false;
+        }
+        return true;
+        
     }
 };
