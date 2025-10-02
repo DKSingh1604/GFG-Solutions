ea13142a5241e32532265c373d11ea9e1e9011f7
Dev Karan Singh <dev1604karan@gmail.com>
2025-10-02 18:28:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,38 +1,83 @@
 class Solution {
   public:
-    void dfs(int node, vector<int> &vis, stack<int> &st, vector<vector<int>> &adj){
+    // void dfs(int node, vector<int> &vis, stack<int> &st, vector<vector<int>> &adj){
         
-        vis[node] = 1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                dfs(it, vis, st, adj);
-            }
-        }
+    //     vis[node] = 1;
+    //     for(auto it: adj[node]){
+    //         if(!vis[it]){
+    //             dfs(it, vis, st, adj);
+    //         }
+    //     }
         
-        st.push(node);
-    }
+    //     st.push(node);
+    // }
+    
+    
+    // vector<int> topoSort(int V, vector<vector<int>>& edges) {
+    //     // code here
+    //     vector<vector<int>> adj(V);
+    //     vector<int> vis(V, 0);
+    //     stack<int> st;
+    //     vector<int> ans;
+    //     for(auto edge : edges){
+    //         int u = edge[0];
+    //         int v = edge[1];
+    //         adj[u].push_back(v);
+    //     }
+        
+    //     for(int i = 0; i < V; i++){
+    //         if(!vis[i]){
+    //             dfs(i, vis, st, adj);
+    //         }
+    //     }
+    //     while(!st.empty()){
+    //         ans.push_back(st.top());
+    //         st.pop();
+    //     }
+        
+    //     return ans;
+    // }
+    
+    // -------------------Kahn's Algo - custom BFS-------------------------
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        // code here
+        
         vector<vector<int>> adj(V);
-        vector<int> vis(V, 0);
-        stack<int> st;
-        vector<int> ans;
         for(auto edge : edges){
             int u = edge[0];
             int v = edge[1];
             adj[u].push_back(v);
         }
         
+        int inDegree[V] = {0};
+        
+        //made the inDegree array
         for(int i = 0; i < V; i++){
-            if(!vis[i]){
-                dfs(i, vis, st, adj);
+            for(auto it : adj[i]){
+                inDegree[it]++;
             }
         }
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        
+        //push the elements with 0 in degree into the queue
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0){
+               q.push(i); 
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.push(it);
+                }
+            }
         }
         
-        return ans;
+        return topo;
     }
 };
