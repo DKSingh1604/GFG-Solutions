f77da56bb832bedc1e52f613c3d4ef9eeb08c38e
Dev Karan Singh <dev1604karan@gmail.com>
2025-10-03 18:36:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -3,46 +3,44 @@ class Solution {
     // Function to find distance of nearest 1 in the grid for each cell.
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
         // Code here
+        
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>> ans(n,vector<int>(m,-1));
-        queue<pair<int,int>> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        queue<pair<pair<int, int>, int>> q; //{{i, j}, distance}
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        vector<vector<int>> dist(n, vector<int> (m, 0));
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
-                    ans[i][j] = 0;
-                    q.push({i,j});
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
         
-        int dist = 0;
-        
-        int dx[4] = {-1,1,0,0};
-        int dy[4] = {0,0,-1,1};
-        
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
         while(!q.empty()){
-            int size = q.size();
-            dist++;
-            while(size--){
-                auto top = q.front();
-                q.pop();
-                
-                int x = top.first;
-                int y = top.second;
-                
-                for(int i=0;i<4;i++){
-                    int nx = dx[i] + x;
-                    int ny = dy[i] + y;
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny] == 0){
-                        ans[nx][ny] = dist;
-                        grid[nx][ny] = 1;
-                        q.push({nx,ny});
-                    }
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            
+            dist[row][col] = steps;
+            
+            for(int i = 0; i < 4; i++){
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+                !vis[nrow][ncol] && grid[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps+1});
                 }
             }
         }
-        return ans;
+        return dist;
+        
     }
 };
