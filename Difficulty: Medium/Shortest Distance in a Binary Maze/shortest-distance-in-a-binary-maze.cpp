f83a5e5ab91082ef0a79c0ee8d1c97cab36ac370
Dev Karan Singh <dev1604karan@gmail.com>
2025-10-11 12:04:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
+++ Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -1,3 +1,5 @@
+// User function Template for C++
+
 class Solution {
   public:
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
@@ -5,37 +7,38 @@ class Solution {
         // code here
         int n = grid.size();
         int m = grid[0].size();
+        if(source.first == destination.first && source.second == destination.second){
+            return 0;
+        }
+        vector<vector<int>> dist(n,vector<int>(m, INT_MAX));
+        int srcRow = source.first;
+        int srcCol = source.second;
+        dist[srcRow][srcCol] = 0;
+        
+        queue<pair<int, pair<int, int>>> pq;
+        
+        pq.push({0, {srcRow, srcCol}});
         
         int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, -1, 0, 1};
+        int delCol[] = {0, 1, 0, -1};
         
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        dist[source.first][source.second] = 0;
-        
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
-                                                        greater <pair<int, pair<int, int>>>>pq;
-        pq.push({0, {source.first, source.second}});
-        
-        while(!pq.empty()) {
-            int distance = pq.top().first;
-            int r = pq.top().second.first;
-            int c= pq.top().second.second;
+        while(!pq.empty()){
+            int newDis = pq.front().first;
+            int newRow = pq.front().second.first;
+            int newCol = pq.front().second.second;
             pq.pop();
             
-            if (r == destination.first && c == destination.second) {
-                return distance;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + delRow[i];
-                int ncol = c + delCol[i];
+            for(int i = 0; i < 4; i++){
+                int nrow = newRow + delRow[i];
+                int ncol = newCol + delCol[i];
                 
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 
-                                            && distance + 1 < dist[nrow][ncol]) {
-                    dist[nrow][ncol] = distance + 1;
-                    pq.push({dist[nrow][ncol], {nrow, ncol}});
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && newDis + 1 < dist[nrow][ncol]){
+                    dist[nrow][ncol] = newDis + 1;
+                    if(nrow == destination.first && ncol == destination.second) return dist[nrow][ncol];
+                    pq.push({newDis + 1,{nrow, ncol}});
                 }
             }
+            
         }
         return -1;
         
