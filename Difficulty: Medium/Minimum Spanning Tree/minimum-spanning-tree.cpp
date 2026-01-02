752d515cef90b2b6c349533ee574ff2564905357
Dev Karan Singh <dev1604karan@gmail.com>
2026-01-02 08:10:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -2,32 +2,37 @@ class Solution {
   public:
     int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
-        vector<vector<pair<int, int>>> adjList(V);
+        vector<vector<pair<int, int>>> adj(V);
         for(auto edge : edges){
-            int from = edge[0];
-            int to = edge[1];
-            int wt = edge[2];
-            adjList[from].push_back({to, wt});
-            adjList[to].push_back({from, wt});
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
         
-        priority_queue<pair<int, int>, 
-        vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        int vis[V] = {0};
-        pq.push({0, 0});//{wt, node}
+        //min heap
+        priority_queue<
+        pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        //(weight, node)
+        pq.push({0, 0});
+        
+        vector<int> vis(V, 0);
         int sum = 0;
+        
         while(!pq.empty()){
-            int node = pq.top().second;
             int wt = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
             
-            if(vis[node] == 1) continue;
-            
-            vis[node] = 1;
-            sum += wt;
-            for(auto it : adjList[node]){
-                if(!vis[it.first]){
-                    pq.push({it.second, it.first});
+            if(!vis[node]){
+                vis[node] = 1;
+                sum += wt;
+                
+                for(auto it : adj[node]){
+                    if(!vis[it.first]){
+                        pq.push({it.second, it.first});
+                    }
                 }
             }
         }
