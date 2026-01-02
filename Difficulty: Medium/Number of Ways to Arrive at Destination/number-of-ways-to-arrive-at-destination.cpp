1a0dc530f5e81dddfa37e773984d9d8f1580ea28
Dev Karan Singh <dev1604karan@gmail.com>
2026-01-02 07:54:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
+++ Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,57 +1,49 @@
 class Solution {
   public:
-    int countPaths(int n, vector<vector<int>>& roads) {
-       
-       vector< pair< int , int > > adj[n] ;
-       
-       for( auto it : roads )
-       {
-           adj[it[0]].push_back({ it[1] , it[2] } ) ;
-           adj[it[1]].push_back( { it[0] , it[2] } ) ;
-       }
-       
-       
-       vector< long long > dist( n , 1e18 ) ;            // edgeweight can be large number so we use long long 
-       vector < int > ways( n , 0 ) ;
-       dist[0] = 0 ;
-       ways[0] = 1 ;
-       
-       priority_queue<  pair< long long , int > , 
-       vector< pair< long long , int >> , 
-       greater< pair< long long ,int >>> pq ;
-       int mod = (int)(1e9 + 7) ;
-       pq.push({0 , 0 }); // { dist , node } 
-       
-       while( !pq.empty() )
-       {
-           long long distance = pq.top().first ;
-           int node = pq.top().second ;
-           pq.pop() ;
-           
-           for( auto it : adj[node] )
-           {
-               int adjnode = it.first ;
-               long long edjwt = it.second ;
-               if( edjwt + distance < dist[adjnode] )
-               {
-                   dist[adjnode] = edjwt + distance ;
-                   pq.push({ edjwt + distance , adjnode } ) ;
-                   ways[adjnode] = ways[node] ;
-               }
-               
-               else if( edjwt + distance == dist[adjnode] )
-               {
-                   ways[adjnode] = ( ways[node] + ways[adjnode] )%mod ;
-               }
-               
-           }
-           
-           
-       }
-       
-       return ways[n-1]%mod ;
-       
-       
-       
+    int countPaths(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int, int>>> adj(V);
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int t = edge[2];
+            adj[u].push_back({v, t});
+            adj[v].push_back({u, t});
+        }
+        
+        priority_queue<
+        pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        //(distance, node)
+        pq.push({0, 0});
+        
+        vector<int> distance(V, INT_MAX);
+        distance[0] = 0;
+        
+        vector<int> ways(V, 0);
+        ways[0] = 1;
+        
+        int mod = (int)(1e9 + 7);
+        
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int newDist = it.second;
+                
+                if(newDist + dis < distance[adjNode]){
+                    distance[adjNode] = newDist + dis;
+                    pq.push({newDist + dis, adjNode});
+                    ways[adjNode] = ways[node];
+                }
+                else if(newDist + dis == distance[adjNode]){
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[V-1] % mod;
+        
     }
 };
