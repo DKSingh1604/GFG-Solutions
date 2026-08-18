5e6cba28387ef29cdbcc96990df9f6b96d4fbec5
Dev Karan Singh <dev1604karan@gmail.com>
2026-08-18 17:26:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Peak element/peak-element.cpp
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -2,19 +2,34 @@ class Solution {
   public:
     int peakElement(vector<int> &arr) {
         // code here
-        int n = arr.size();
-        if(n == 1) return 0;
-        if(arr[0] > arr[1]) return 0;
         
-        for(int i = 1; i < n; i++){
-            if(i == n-1){
-                continue;
-            }
+        //Applying BINARY SEARCH
+        
+        int n = arr.size();
+        //if the array has only 1 element OR the first element is the peak
+        if((n == 1) || (arr[0] > arr[1])) return 0;
+        
+        //if the last element is the peak
+        if(arr[n-1] > arr[n-2]) return n-1;
+        
+        int l = 1;
+        int r = n-2;
+        
+        while(l <= r){
+            int mid  = l + (r-l)/2;
             
-            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-                return i;
+            //base case - if mid is the peak
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
+            
+            //if the peak is to the right of mid
+            if(arr[mid] > arr[mid-1]){
+                l = mid + 1;
+            }
+            else{
+                r = mid;
             }
         }
         return n-1;
+        
     }
 };
