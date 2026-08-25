97fbb774113bcbea002078b973f368ee362567a2
Dev Karan Singh <dev1604karan@gmail.com>
2026-08-25 23:34:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+        
+        //mark rows and col in the respective array
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        
+        //reiterate over the mat and replace the value
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(row[i] || col[j]){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+};
