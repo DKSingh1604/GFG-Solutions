1584ec89166e904852f129f34444884f8bc53dd6
Dev Karan Singh <dev1604karan@gmail.com>
2026-09-15 12:35:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distinct Permutations of a String/distinct-permutations-of-a-string.cpp
+++ Difficulty: Medium/Distinct Permutations of a String/distinct-permutations-of-a-string.cpp
@@ -1,32 +1,36 @@
 class Solution {
-  public:
-    vector<string> ans;
-    
-    void solve(int index, string &s){
-        
-        //base case
-        if(index >= s.size()) {
-            ans.push_back(s);
-            return;
-        }
-        set<char> used;
-        
-        for(int j = index; j < s.size(); j++){
-            
-            if(used.count(s[j])) continue;
-            
-            used.insert(s[j]);
-            
-            swap(s[j], s[index]);
-            solve(index+1, s);
-            swap(s[j], s[index]);
-        }
-    }
-    vector<string> findPermutation(string &s) {
-        
-        ans.clear();
-    
-        solve(0, s);
-        return ans;
-    }
+	public:
+	vector<string> ans;
+	void solve(int index, string &s) {
+		
+		// base case
+		if (index >= s.size()) {
+			ans.push_back(s);
+			return;
+		}
+		set<char> used;
+		
+		for (int i = index; i < s.size(); i++) {
+			
+			if (used.count(s[i])) {
+				continue;
+			}
+			
+			used.insert(s[i]);
+			
+			swap(s[i], s[index]);
+			solve(index + 1, s);
+			// bactracking - so the string remains the same for next iteration
+			swap(s[i], s[index]);
+		}
+		
+	}
+	
+	vector<string> findPermutation(string &s) {
+		// Code here
+		ans.clear();
+		solve(0, s);
+		return ans;
+		
+	}
 };
