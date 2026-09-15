f1b569ca790a95c563dbc2a0006d93f7686b01bd
Dev Karan Singh <dev1604karan@gmail.com>
2026-09-15 13:58:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Target Sum Combinations/target-sum-combinations.cpp
+++ Difficulty: Medium/Target Sum Combinations/target-sum-combinations.cpp
@@ -1,30 +1,34 @@
 class Solution {
 	public:
-	void solve(int index, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-	    
-	    //base case
-	    if(index == arr.size()){
-	        if(target == 0){
-	            ans.push_back(ds);
-	        }
-	        return;
-	    }
-	    
-	    //pick up the element
-	    if(arr[index] <= target){
-	        ds.push_back(arr[index]);
-	        solve(index, target - arr[index], arr, ans, ds);
-	        ds.pop_back();
-	    }
-	    
-	    solve(index+1, target, arr, ans, ds);
-	    
+	vector<vector<int>> ans;
+	vector<int> ds;
+	void solve(int index, int target, vector<int> &arr) {
+		// 0  1  2 - indices
+		// 1  2  3 - array, target - 5
+		
+		// base case
+		if (index == arr.size()) {
+			if (target == 0) {
+				ans.push_back(ds);
+				
+			}
+			return;
+		}
+		
+		//recursive condition
+		if(arr[index] <= target){
+		    ds.push_back(arr[index]);
+		    solve(index, target-arr[index], arr);
+		    ds.pop_back();
+		}
+		solve(index+1, target, arr);
+		
 	}
 	vector<vector<int>> targetSumComb(vector<int> &arr, int target) {
 		// code here
-		vector<vector<int>> ans;
-		vector<int> ds;
-		solve(0, target, arr, ans, ds);
+		
+		solve(0, target, arr);
 		return ans;
+		
 	}
 };
